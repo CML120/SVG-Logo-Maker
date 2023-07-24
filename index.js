6c98ae3ea6655591c6a86b993619d0d6bb9c49cb
CML120 <cheelor922@gmail.com>
2023-07-24 11:33:58 -0500

    changed inquerer to version 8.2.4, added code for initial prompt and inputs

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
+// packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Validate if the input is a valid color
+function isValidColor(input) {
+    const colorRegex = /^(#[0-9a-fA-F]{6}|[a-zA-Z]+)$/;
+    return colorRegex.test(input);
+  }
+  
+function questionsPrompt () {
+    inquirer    
+        .prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'Enter up to 3 characters:',
+                validate: (input => input.length <= 3 ? true : 'Please enter up to 3 characters')
+            },
+        
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'Enter text color:',
+                validate: (input) => isValidColor(input) ? true : 'Please enter a valid color.',
+              },
+              {
+                type: 'list',
+                name: 'shape',
+                message: 'Choose a shape:',
+                choices: ['circle', 'triangle', 'square'],
+              },
+              {
+                type: 'input',
+                name: 'shapeColor',
+                message: 'Enter shape color',
+                validate: (input) => isValidColor(input) ? true : 'Invalid color. Please enter a valid color',
+              },
+        ])
+}
+
+
+
+
+function init() {
+    console.log(`
+================================================================================================================================================
+
+                                                                                                                                        
+      _/_/_/  _/      _/    _/_/_/      _/                                          _/      _/            _/                            
+   _/        _/      _/  _/            _/          _/_/      _/_/_/    _/_/        _/_/  _/_/    _/_/_/  _/  _/      _/_/    _/  _/_/   
+    _/_/    _/      _/  _/  _/_/      _/        _/    _/  _/    _/  _/    _/      _/  _/  _/  _/    _/  _/_/      _/_/_/_/  _/_/        
+       _/    _/  _/    _/    _/      _/        _/    _/  _/    _/  _/    _/      _/      _/  _/    _/  _/  _/    _/        _/           
+_/_/_/        _/        _/_/_/      _/_/_/_/    _/_/      _/_/_/    _/_/        _/      _/    _/_/_/  _/    _/    _/_/_/  _/            
+                                                             _/                                                                         
+                                                        _/_/
+
+                                                  Welcome to the SVG Logo Maker App! 
+                           Please enter information into the following prompts to generate your logo.
+================================================================================================================================================
+    `);
+
+    questionsPrompt()
+
+}
+
+init();
